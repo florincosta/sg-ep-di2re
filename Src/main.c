f6d71724b8eb2c878efbfde8967a121a95500062
Florin Costa <florin.costa83@gmail.com>
2018-04-26 14:09:18 +0300

    changed micro to STM32L082KZT6, added README with semihosting info

--- Src/main.c
+++ Src/main.c
@@ -133,7 +133,7 @@ RTC_HandleTypeDef hrtc;
 
 SPI_HandleTypeDef hspi1;
 
-UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
@@ -161,9 +161,12 @@ static void MX_GPIO_Init(void);
 static void MX_ADC_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_RTC_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
+
+extern void initialise_monitor_handles(void);
+
 /* Private function prototypes -----------------------------------------------*/
 /*!
  * \brief Function to be executed on Radio Tx Done event
@@ -204,6 +207,7 @@ void OnRxError( void );
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+    initialise_monitor_handles();
 
   /* USER CODE END 1 */
 
@@ -228,7 +232,7 @@ int main(void)
   MX_ADC_Init();
   MX_SPI1_Init();
   MX_RTC_Init();
-  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
 
@@ -291,6 +295,8 @@ int main(void)
 
       HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 
+      printf("test\n");
+
   }
   /* USER CODE END 3 */
 
@@ -338,8 +344,8 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_RTC;
-  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_RTC;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
@@ -444,21 +450,21 @@ static void MX_SPI1_Init(void)
 
 }
 
-/* USART2 init function */
-static void MX_USART2_UART_Init(void)
+/* USART1 init function */
+static void MX_USART1_UART_Init(void)
 {
 
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_7B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_7B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
--- startup/startup_stm32l082xx.s
+++ startup/startup_stm32l082xx.s
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file      startup_stm32l031xx.s
+  * @file      startup_stm32l082xx.s
   * @author    MCD Application Team
-  * @brief     STM32L031xx Devices vector table for gcc.
+  * @brief     STM32L082xx Devices vector table for Atollic TrueSTUDIO toolchain.
   *            This module performs:
   *                - Set the initial SP
   *                - Set the initial PC == Reset_Handler,
@@ -154,34 +154,34 @@ g_pfnVectors:
   .word     PVD_IRQHandler                    /* PVD through EXTI Line detection */
   .word     RTC_IRQHandler                    /* RTC through the EXTI line     */
   .word     FLASH_IRQHandler                  /* FLASH                        */
-  .word     RCC_IRQHandler                    /* RCC                          */
+  .word     RCC_CRS_IRQHandler                /* RCC and CRS                  */
   .word     EXTI0_1_IRQHandler                /* EXTI Line 0 and 1            */
   .word     EXTI2_3_IRQHandler                /* EXTI Line 2 and 3            */
   .word     EXTI4_15_IRQHandler               /* EXTI Line 4 to 15            */
-  .word     0                                 /* Reserved                     */
+  .word     TSC_IRQHandler                     /* TSC                           */
   .word     DMA1_Channel1_IRQHandler          /* DMA1 Channel 1               */
   .word     DMA1_Channel2_3_IRQHandler        /* DMA1 Channel 2 and Channel 3 */
   .word     DMA1_Channel4_5_6_7_IRQHandler    /* DMA1 Channel 4, Channel 5, Channel 6 and Channel 7*/
   .word     ADC1_COMP_IRQHandler              /* ADC1, COMP1 and COMP2        */
   .word     LPTIM1_IRQHandler                 /* LPTIM1                       */
-  .word     0                                 /* Reserved                     */
+  .word     USART4_5_IRQHandler               /* USART4 and USART 5           */
   .word     TIM2_IRQHandler                   /* TIM2                         */
-  .word     0                                 /* Reserved                     */
-  .word     0                                 /* Reserved                     */
-  .word     0                                 /* Reserved                     */
-  .word     0                                 /* Reserved                     */
+  .word     TIM3_IRQHandler                   /* TIM3                         */
+  .word     TIM6_DAC_IRQHandler               /* TIM6 and DAC                 */
+  .word     TIM7_IRQHandler 				          /* TIM7                         */
+  .word     0              					          /* Reserved                     */
   .word     TIM21_IRQHandler                  /* TIM21                        */
-  .word     0                                 /* Reserved                     */
+  .word     I2C3_IRQHandler                   /* I2C3                         */
   .word     TIM22_IRQHandler                  /* TIM22                        */
   .word     I2C1_IRQHandler                   /* I2C1                         */
-  .word     0                                 /* Reserved                     */
+  .word     I2C2_IRQHandler                   /* I2C2                         */
   .word     SPI1_IRQHandler                   /* SPI1                         */
-  .word     0                                 /* Reserved                     */
-  .word     0                                 /* Reserved                     */
+  .word     SPI2_IRQHandler                   /* SPI2                         */
+  .word     USART1_IRQHandler                 /* USART1                       */
   .word     USART2_IRQHandler                 /* USART2                       */
-  .word     LPUART1_IRQHandler                /* LPUART1                      */
+  .word     AES_RNG_LPUART1_IRQHandler        /* AES, RNG and LPUART1         */
   .word     0                                 /* Reserved                     */
-  .word     0                                 /* Reserved                     */
+  .word     USB_IRQHandler                    /* USB                          */
 
 /*******************************************************************************
 *
@@ -218,8 +218,8 @@ g_pfnVectors:
    .weak      FLASH_IRQHandler
    .thumb_set FLASH_IRQHandler,Default_Handler
 
-   .weak      RCC_IRQHandler
-   .thumb_set RCC_IRQHandler,Default_Handler
+   .weak      RCC_CRS_IRQHandler
+   .thumb_set RCC_CRS_IRQHandler,Default_Handler
 
    .weak      EXTI0_1_IRQHandler
    .thumb_set EXTI0_1_IRQHandler,Default_Handler
@@ -229,6 +229,9 @@ g_pfnVectors:
 
    .weak      EXTI4_15_IRQHandler
    .thumb_set EXTI4_15_IRQHandler,Default_Handler
+
+   .weak      TSC_IRQHandler
+   .thumb_set TSC_IRQHandler,Default_Handler
 
    .weak      DMA1_Channel1_IRQHandler
    .thumb_set DMA1_Channel1_IRQHandler,Default_Handler
@@ -245,11 +248,26 @@ g_pfnVectors:
    .weak      LPTIM1_IRQHandler
    .thumb_set LPTIM1_IRQHandler,Default_Handler
 
+   .weak      USART4_5_IRQHandler
+   .thumb_set USART4_5_IRQHandler,Default_Handler
+
    .weak      TIM2_IRQHandler
    .thumb_set TIM2_IRQHandler,Default_Handler
 
+   .weak      TIM3_IRQHandler
+   .thumb_set TIM3_IRQHandler,Default_Handler
+
+   .weak      TIM6_DAC_IRQHandler
+   .thumb_set TIM6_DAC_IRQHandler,Default_Handler
+
+   .weak      TIM7_IRQHandler
+   .thumb_set TIM7_IRQHandler,Default_Handler
+
    .weak      TIM21_IRQHandler
    .thumb_set TIM21_IRQHandler,Default_Handler
+
+   .weak      I2C3_IRQHandler
+   .thumb_set I2C3_IRQHandler,Default_Handler
 
    .weak      TIM22_IRQHandler
    .thumb_set TIM22_IRQHandler,Default_Handler
@@ -257,15 +275,26 @@ g_pfnVectors:
    .weak      I2C1_IRQHandler
    .thumb_set I2C1_IRQHandler,Default_Handler
 
+   .weak      I2C2_IRQHandler
+   .thumb_set I2C2_IRQHandler,Default_Handler
+
    .weak      SPI1_IRQHandler
    .thumb_set SPI1_IRQHandler,Default_Handler
+
+   .weak      SPI2_IRQHandler
+   .thumb_set SPI2_IRQHandler,Default_Handler
+
+   .weak      USART1_IRQHandler
+   .thumb_set USART1_IRQHandler,Default_Handler
 
    .weak      USART2_IRQHandler
    .thumb_set USART2_IRQHandler,Default_Handler
 
-   .weak      LPUART1_IRQHandler
-   .thumb_set LPUART1_IRQHandler,Default_Handler
+   .weak      AES_RNG_LPUART1_IRQHandler
+   .thumb_set AES_RNG_LPUART1_IRQHandler,Default_Handler
 
+   .weak      USB_IRQHandler
+   .thumb_set USB_IRQHandler,Default_Handler
 
 
 
